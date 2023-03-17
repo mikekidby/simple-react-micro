9407d645015191f713e84c66d8c577c1b78c8f7d
Michael Kidby <mkidby@wiley.com>
2023-03-17 13:58:24 -0500

    api upload simple micro arc

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json())
+app.use(cors());
 
 const posts = {};
 
@@ -25,4 +27,4 @@ app.post('/posts', (req, res) =>{
 
 app.listen(4000, () => {
     console.log('Listening on 4000');
-});
+}); 
