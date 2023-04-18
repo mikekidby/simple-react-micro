f80f16c03ca972d8749468b197240e31f6932212
Michael Kidby <mkidby@wiley.com>
2023-04-17 21:00:07 -0500

    feat(skaffold): automate k8s with skaffold

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -4,20 +4,18 @@ const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     let content;
 
-    switch (comment.status){
-        case 'approved':
-            content = comment.content;
-            break;
-        case 'pending':
-            content = 'This comment is awaiting moderation';
-            break;
-        case 'rejected':
-            content = 'This comment has been rejected';
-            break;
-        default:
-            content = 'error';
+    if (comment.status === "approved") {
+      content = comment.content;
     }
-  
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
     return <li key={comment.id}>{content}</li>;
   });
 
--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -11,7 +11,7 @@ app.post('/events', async (req, res) => {
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentModerated',
       data: {
         id: data.id,
@@ -28,4 +28,3 @@ app.post('/events', async (req, res) => {
 app.listen(4003, () => {
   console.log('Listening on 4003');
 });
- 
